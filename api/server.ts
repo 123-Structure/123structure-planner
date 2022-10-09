23961204b34cc7856dba8188052e55eddc9b2dc1
Baptiste LECHAT <baptistelechat@outlook.fr>
2022-10-09 19:59:09 +0200

    ✨ Configuration of routes to request projects from the API

--- api/server.ts
+++ api/server.ts
@@ -1,15 +1,28 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
+import projectsRouter from "./routes/projects";
 
 dotenv.config();
 
+// Express App
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server is running !");
+// Middleware
+app.use(express.json());
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(req.path, req.method);
+  next();
 });
 
+// Routes
+app.get("/", (req: Request, res: Response) => {
+  res.json({ msg: "🌍 Welcome to the app !" });
+});
+
+app.use("/api/projects", projectsRouter);
+
+// Listen requests
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
 });
