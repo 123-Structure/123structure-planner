95c8b2a325a1463ff193f4db7f9a54a0594d4fcb
baptistelechat <baptistelechat@outlook.fr>
2023-02-21 17:34:55 +0100

    ✨ add an icon to indicate the possibility to update the logo of a client in edit mode

--- frontend/src/components/Commercial/components/Customer/components/CustomerIdentity.tsx
+++ frontend/src/components/Commercial/components/Customer/components/CustomerIdentity.tsx
@@ -7,7 +7,7 @@ import {
   IconMap2,
   IconPhone,
 } from "@tabler/icons";
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import {
   useCustomer,
   useUpdateCustomer,
@@ -25,6 +25,7 @@ import Contact from "../../utils/Contact";
 import CustomerItem from "../../utils/CustomerItem";
 import CustomerContact from "./CustomerContact";
 import House from "../../../../../assets/img/house.png";
+import { useSetState } from "@mantine/hooks";
 
 interface ICustomerIdentityProps {
   customer: ICustomer;
@@ -38,6 +39,8 @@ const CustomerIdentity = (props: ICustomerIdentityProps) => {
   const [email, setEmail] = useState(props.customer.email);
   const [phone, setPhone] = useState(props.customer.phone);
   const [contact, setContact] = useState(props.customer.contact);
+
+  const [logo, setLogo] = useState(props.customer.logo);
 
   const customers = useCustomer();
   const setCustomers = useUpdateCustomer();
@@ -67,6 +70,7 @@ const CustomerIdentity = (props: ICustomerIdentityProps) => {
     changedCustomer[0].email = email;
     changedCustomer[0].phone = phone;
     changedCustomer[0].contact = contact;
+    changedCustomer[0].logo = logo;
 
     setCustomers(newCustomer);
     showNotification({
@@ -84,12 +88,30 @@ const CustomerIdentity = (props: ICustomerIdentityProps) => {
     setEmail(props.customer.email);
     setPhone(props.customer.phone);
     setContact(props.customer.contact);
+    setLogo(props.customer.logo);
     showNotification({
       title: `⛔ Fiche client non sauvegardée`,
       message: `Les modifications pour ${props.customer.name} sont annulées`,
       color: "red",
     });
     setEditCustomerIdentity(false);
+  };
+
+  const handleUploadFile = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) {
+      return;
+    }
+
+    const file = e.target.files[0];
+
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.onload = () => {
+      const base64String = fileReader.result as string;
+      if (base64String !== null) {
+        setLogo(base64String);
+      }
+    };
   };
 
   return (
@@ -120,20 +142,27 @@ const CustomerIdentity = (props: ICustomerIdentityProps) => {
         />
       </div>
       <div className="customerIdentityContainer">
-        <div>
+        <div
+          className={`customerLogoContainer ${
+            editCustomerIdentity ? "editCustomerLogo" : ""
+          }`}
+          onClick={() =>
+            editCustomerIdentity
+              ? document.getElementById("logoFileInput")?.click()
+              : null
+          }
+        >
           <img
-            className={`customerLogo ${
-              editCustomerIdentity ? "editCustomerLogo" : ""
-            }`}
-            src={props.customer.logo !== "" ? props.customer.logo : House}
+            className="customerLogo"
+            src={logo !== "" ? logo : House}
             alt={`Logo ${props.customer.name}`}
-            onClick={() =>
-              editCustomerIdentity
-                ? document.getElementById("logoFileInput")?.click()
-                : null
-            }
           />
-          <input type="file" id="logoFileInput" />
+          <input
+            type="file"
+            id="logoFileInput"
+            onChange={(e) => handleUploadFile(e)}
+            accept="image/png,image/jpeg"
+          />
         </div>
         <div
           className="customerItemContainer"
